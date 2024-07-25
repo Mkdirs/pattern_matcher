288df522d307ff89132c87b5ec6954c9934c4429
Hafez Ben-Abdillah MADI <hafezbenabdillah.madi@gmail.com>
2024-07-25 19:17:42 +0200

    Giga commit

---
+++ src/lib.rs
@@ -0,0 +1,343 @@
+use std::{error::Error, fmt::{Debug, Display}, num::NonZeroUsize, process::Termination};
+
+#[cfg(test)]
+mod tests;
+
+/// A struct representing a group of symbols
+#[derive(Debug, PartialEq, Clone)]
+pub struct SymbolGroup<'a, Symbol>{
+    pub accepted_symbols: &'a [Symbol],
+    pub description: &'a str
+}
+
+impl<'a, Symbol> SymbolGroup<'a, Symbol> where Symbol: Debug+PartialEq{
+        /// Tells if a symbol is a part of the group or not
+        pub fn accept(&self, symbol: &Symbol) -> bool {
+            self.accepted_symbols.contains(symbol)
+        }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+/// This structure helps you building a pattern matching pipeline
+pub struct MatchingPipeline<Symbol> where Symbol: PartialEq+Clone{
+    matched: Vec<Symbol>,
+    unmatched: Vec<Symbol>,
+    reached_eos: bool
+}
+
+impl <Symbol> Termination for MatchingPipeline<Symbol> where Symbol: PartialEq+Clone{
+    fn report(self) -> std::process::ExitCode {
+        std::process::ExitCode::SUCCESS
+    }
+}
+
+impl From<&str> for MatchingPipeline<char>{
+    fn from(value: &str) -> Self {
+        MatchingPipeline::new(value.chars())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum PipelineError<'a, Symbol> where Symbol: Debug{
+    UnexpectedEos,
+    WrongSymbol{
+        expected: &'a Symbol,
+        actual: Symbol
+    },
+    WrongPattern{
+        expected: &'a [Symbol],
+        actual: Vec<Symbol>
+    },
+
+    SymbolNotPartOfGroup{
+        expected: SymbolGroup<'a, Symbol>,
+        actual: Symbol
+    },
+
+    SymbolNotMatchAnyOf{
+        expected: &'a [Symbol],
+        actual: Symbol
+    }
+
+}
+impl<'a, Symbol> Display for PipelineError<'a, Symbol> where Symbol: Debug+PartialEq{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self{
+            &Self::UnexpectedEos => write!(f, "Unexpected end of stream"),
+            &Self::WrongSymbol { expected, actual } => write!(f, "Expected {expected:?} but instead got {actual:?}"),
+            &Self::WrongPattern { expected, actual } => write!(f, "Expected pattern {expected:?} but instead got {actual:?}"),
+            &Self::SymbolNotMatchAnyOf { expected, actual } => write!(f, "Expected one of {expected:?} but instead got {actual:?}"),
+            &Self::SymbolNotPartOfGroup { expected, actual } => write!(f, "Expected one of {0:?} but instead got {actual:?}", expected.description)
+        }
+    }
+}
+impl<'a, Symbol> Error for PipelineError<'a, Symbol> where Symbol: Debug+PartialEq{}
+
+pub type PipelineResult<'a, Symbol> = Result<MatchingPipeline<Symbol>, PipelineError<'a, Symbol>>;
+
+trait Quantifier{}
+
+pub struct Exactly(pub NonZeroUsize); impl Quantifier for Exactly{}
+/*struct ZeroOrOne; impl Quantifier for ZeroOrOne{}
+struct AtLeast(usize); impl Quantifier for AtLeast{}
+struct AtMost(usize); impl Quantifier for AtMost{}*/
+
+pub trait Quantifiable<'a, Q:Quantifier, Symbol>:Sized where Symbol: Debug+PartialEq+Clone{
+    fn with_quantifier<F>(self, quantifier:Q, callback: F) -> PipelineResult<'a, Symbol> where F: Fn(Self) -> PipelineResult<'a, Symbol>;
+}
+
+impl<'a, Symbol> Quantifiable<'a, Exactly, Symbol> for MatchingPipeline<Symbol> where Symbol: PartialEq+Clone+Debug {
+    fn with_quantifier<F>(mut self, quantifier:Exactly, callback: F) -> PipelineResult<'a, Symbol> where F: Fn(Self) -> PipelineResult<'a, Symbol> {
+
+        let mut n:Option<NonZeroUsize> = None;
+        loop{
+
+            if self.reached_eos {
+                break;
+            }
+
+            let pipeline = self.clone();
+
+            let result = callback(pipeline);
+
+            match result {
+                Ok(s) => {
+                    self = s;
+                    if n.is_none() {
+                        n = Some(NonZeroUsize::new(1).unwrap())
+                    }else{
+                        n = n.and_then(|x| x.checked_add(1));
+                    }
+                    
+                    // On est sûr que n vaut Some(...)
+                    let n = n.unwrap();
+
+                    if n == quantifier.0 {
+                        return Ok(self);
+                    }else if n < quantifier.0 {
+                        continue;
+                    }/*else {
+                        return Err(format!("Unexpected: {0:?}", self.unmatched.get(0)));
+                    }*/
+                },
+
+                Err(_) => return result
+            }
+
+        }
+
+        Err(PipelineError::UnexpectedEos)
+        
+    }
+}
+
+
+
+impl<'a, Symbol> MatchingPipeline<Symbol> where Symbol: PartialEq+Clone+Debug+Copy{
+    fn new(candidate: impl Iterator<Item = Symbol>) -> Self{
+        let collection = candidate.collect::<Vec<Symbol>>();
+        Self { matched: vec![], reached_eos: collection.is_empty(), unmatched: collection  }
+    }
+
+    /// Matches the current symbol:
+    /// 
+    /// The symbol is added to the list of matched symbols
+    /// and the pipeline moves to the next symbol of the sequence
+    pub fn consume(mut self) -> Self {
+        if self.reached_eos {
+            return self;
+        }
+
+        let (matched, unmatched) = self.unmatched.split_at(1);
+
+        self.matched.append(&mut matched.to_vec());
+        self.unmatched = unmatched.to_vec();
+
+        self.reached_eos = self.unmatched.is_empty();
+
+        self
+    }
+
+    /// Moves the pipeline to the next symbol of the sequence
+    /// 
+    /// The current symbol is not added to the matched symbols list
+    pub fn skip(mut self) -> Self {
+        if self.reached_eos {
+            return self;
+        }
+
+        self.unmatched = self.unmatched.get(1..).unwrap_or_default().to_vec();
+
+        self.reached_eos = self.unmatched.is_empty();
+
+        self
+    }
+
+    /// Expects that `symbol` can be matched
+    /// 
+    /// * `symbol` - The expected symbol
+    pub fn match_symbol(self, symbol:&'a Symbol) -> PipelineResult<'a, Symbol>{
+        if self.reached_eos {
+            return Err(PipelineError::UnexpectedEos);
+        }
+
+        let actual = self.unmatched.get(0).unwrap().clone();
+        if symbol == &actual {
+            return  Ok(self.consume());
+        }
+
+        Err(PipelineError::WrongSymbol { expected: &symbol, actual})
+        
+    }
+
+    /// Expects that `pattern` can be matched
+    /// 
+    /// * `pattern` - The expected pattern
+    pub fn match_pattern(mut self, pattern:&'a [Symbol]) -> PipelineResult<'a, Symbol>{
+        let pipeline = self.clone();
+        match pipeline.unmatched.get(0..pattern.len()) {
+            Some(symbols) if symbols == pattern => {
+                for symbol in symbols{
+                    //Todo: replace with consume()
+                    self = self.match_symbol(symbol).expect("All symbols should match");
+                }
+
+                Ok(self)
+            },
+
+            Some(s) => {
+                Err(PipelineError::WrongPattern { expected: pattern, actual: s.to_vec() })
+            },
+
+            None => Err(PipelineError::WrongPattern { expected: pattern, actual: pipeline.unmatched.to_vec() })
+        }
+    }
+
+    /// Expects that `symbols` contains the current symbol 
+    /// 
+    /// * `symbols` - A list of symbols
+    pub fn match_any_of(mut self, symbols:&'a [Symbol]) -> PipelineResult<'a, Symbol> {
+        if self.reached_eos {
+            return Err(PipelineError::UnexpectedEos);
+        }
+        let actual = self.unmatched.get(0).unwrap().clone();
+
+        if symbols.contains(&actual) {
+
+            self = self.consume();
+
+            return Ok(self);
+        }
+
+        Err(PipelineError::SymbolNotMatchAnyOf { expected: symbols, actual })
+    }
+
+    /// Expects that the current symbol is part of [Group](SymbolGroup)
+    pub fn match_any_of_group(mut self, group: SymbolGroup<'a, Symbol>) -> PipelineResult<'a, Symbol>{
+        if self.reached_eos {
+            return Err(PipelineError::UnexpectedEos);
+        }
+
+        let actual = self.unmatched.get(0).unwrap().clone();
+        if group.accept(&actual) {
+
+            self = self.consume();
+
+            return Ok(self);
+        }
+
+        Err(PipelineError::SymbolNotPartOfGroup { expected: group, actual })
+    }
+
+
+    /// Matches all symbols until it matches the pattern `delim` or reaches end of stream
+    /// 
+    /// `delim` is also matched
+    /// 
+    /// * `delim` - The delimiter pattern
+    pub fn match_until(mut self, delim:&[Symbol]) -> PipelineResult<'a, Symbol> {
+    
+        loop {
+            if self.reached_eos {
+                break;
+            }
+
+            let pipeline = self.clone();
+            
+            if let Ok(s) = pipeline.match_pattern(delim){
+                self = s;
+                break;
+            }
+            self = self.consume();
+
+            
+        }
+
+        Ok(self)
+    }
+
+    /// Matches all symbols until one is part of [Group](SymbolGroup) or reaches end of stream
+    /// 
+    /// The delimiting symbol is also matched
+    pub fn match_until_group<Group>(mut self, group: &SymbolGroup<'a, Symbol>) -> PipelineResult<'a, Symbol> {
+        loop {
+            if self.reached_eos {
+                break;
+            }
+
+            let pipeline = self.clone();
+            let symbol = pipeline.unmatched.get(0).expect("Should not be empty");
+            if group.accept(symbol) {
+                self = pipeline.consume();
+                break;
+            }
+            
+            self = pipeline.consume();
+        }
+
+        Ok(self)
+    }
+
+    /// Encapsulates the logic inside a closure
+    pub fn block<F>(self, callback: F) -> PipelineResult<'a, Symbol> where F: Fn(Self) -> PipelineResult<'a, Symbol> {
+        callback(self)
+    }
+
+    
+}
+
+
+/// Creates a [MatchingPipeline]
+pub fn begin_match<U>(candidate: impl Into<MatchingPipeline<U>>) -> MatchingPipeline<U> where U:PartialEq+Clone{
+    candidate.into()
+}
+
+
+
+
+
+
+
+/*
+PatternMatcher
+match_xxx => MatchedPattern
+let str = "-12.24"
+
+begin_match(str)
+    .match_symbol('r')
+
+let (otherDataType, undigestedPattern) = str.match_symbol('-')
+    .fail(|| )
+    .then()
+        .match_digit(Quantifier::OneOrMany)
+        .match_symbol('.')
+        .match_digit(Quantifier::OneOrMany)
+        .digest()
+        .map(|digested_pattern| OtherDataType)
+
+match_symbol('-')
+    .fail(||)
+    .match_digit()
+    .is_complete()
+
+*/
---
+++ src/tests.rs
@@ -0,0 +1,221 @@
+use std::num::NonZeroUsize;
+
+use crate::{begin_match, Exactly, MatchingPipeline, PipelineError, Quantifiable, SymbolGroup};
+
+#[test]
+fn should_match_all_symbols() -> Result<(), PipelineError<'static, char>>{
+    let result = begin_match("hello")
+        .match_symbol(&'h')?
+        .match_symbol(&'e')?
+        .match_symbol(&'l')?
+        .match_symbol(&'l')?
+        .match_symbol(&'o')?;
+
+    let expected = MatchingPipeline{
+        matched: vec!['h', 'e', 'l', 'l', 'o'],
+        unmatched: vec![],
+        reached_eos: true
+    };
+
+    assert_eq!(result, expected);
+
+    Ok(())
+}
+
+#[test]
+fn should_not_match_symbol() -> Result<(), PipelineError<'static, char>> {
+    let result = begin_match("Fou")
+        .match_symbol(&'F')?
+        .match_symbol(&'o')?
+        .match_symbol(&'o');
+
+    let expected = Err(PipelineError::WrongSymbol { expected: &'o', actual: 'u' });
+
+    assert_eq!(result, expected);
+    Ok(())
+}
+
+#[test]
+fn should_not_reach_eos() -> Result<(), PipelineError<'static, char>>{
+    let result = begin_match("Foxy")
+        .match_symbol(&'F')?
+        .match_symbol(&'o')?;
+
+    let expected = MatchingPipeline{
+        matched: vec!['F', 'o'],
+        unmatched: vec!['x', 'y'],
+        reached_eos: false
+    };
+
+    assert_eq!(result, expected);
+
+    Ok(())
+}
+
+#[test]
+fn should_match_pattern() -> Result<(), PipelineError<'static, char>>{
+    let result = begin_match("0x85ADG Header")
+        .match_pattern(&['0','x','8','5','A','D','G'])?;
+
+    let expected = MatchingPipeline{
+        matched: vec!['0','x','8','5','A','D','G'],
+        unmatched: vec![' ','H','e','a','d','e','r'],
+        reached_eos: false
+    };
+
+    assert_eq!(result, expected);
+    Ok(())
+}
+
+#[test]
+fn should_not_match_pattern() {
+    let pattern = &['0','x','8','6','A','D','G'];
+    let result = begin_match("0x85ADG Header")
+        .match_pattern(pattern);
+
+    let expected = Err(PipelineError::WrongPattern { expected: pattern, actual: vec!['0','x','8','5','A','D','G'] });
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn pattern_is_too_big() {
+    let pattern = &['0','x','8','6','A','D','G'];
+    let result = begin_match("0x")
+        .match_pattern(pattern);
+
+    let expected = Err(PipelineError::WrongPattern { expected: pattern, actual: vec!['0', 'x'] });
+
+    assert_eq!(result, expected);
+}
+
+#[test]
+fn should_match_until_comma() -> Result<(), PipelineError<'static, char>>{
+    let result = begin_match("Foo,Bar ,baz")
+        .match_until(&[','])?;
+
+    let expected = MatchingPipeline{
+        matched: vec!['F','o','o',','],
+        unmatched: vec!['B','a','r',' ',',','b','a','z'],
+        reached_eos: false
+    };
+
+    assert_eq!(result, expected);
+    
+    Ok(())
+}
+
+#[test]
+fn should_skip() {
+    let result = begin_match("Fax")
+        .consume()
+        .skip()
+        .consume();
+
+    let expected = MatchingPipeline{
+        matched: vec!['F', 'x'],
+        unmatched: vec![],
+        reached_eos: true
+    };
+
+    assert_eq!(result, expected);
+
+}
+
+#[test]
+fn should_match_any() -> Result<(), PipelineError<'static, char>>{
+    let digits = &['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    let result = begin_match("1 2 3")
+        .match_any_of(digits)?
+        .skip()
+        .match_any_of(digits)?
+        .skip()
+        .match_any_of(digits)?;
+
+    let expected = MatchingPipeline{
+        matched: vec!['1', '2', '3'],
+        unmatched: vec![],
+        reached_eos: true
+    };
+
+    assert_eq!(result, expected);
+
+    Ok(())
+}
+
+#[test]
+fn should_match_any_group() -> Result<(), PipelineError<'static, char>>{
+    let terminator = SymbolGroup{
+        accepted_symbols: &[',', ';'],
+        description: "',' or ';'"
+    };
+
+    let result = begin_match("a;b,1")
+        .skip()
+        .match_any_of_group(terminator.clone())?
+        .skip()
+        .match_any_of_group(terminator)?
+        .skip();
+
+    let expected = MatchingPipeline{
+        matched: vec![';', ','],
+        unmatched: vec![],
+        reached_eos: true
+    };
+
+    assert_eq!(result, expected);
+
+    Ok(())
+        
+}
+
+#[test]
+fn state_should_be_preserved_in_block() -> Result<(), PipelineError<'static, char>>{
+    let result = begin_match("abcFoo1Bar2")
+        .match_symbol(&'a')?.match_symbol(&'b')?.match_symbol(&'c')?
+        .block(|p| {
+            p.match_pattern(&['F', 'o', 'o', '1'])?
+            .match_symbol(&'B')
+        })?.match_symbol(&'a')?;
+
+    let expected = MatchingPipeline{
+        matched: vec!['a', 'b', 'c', 'F', 'o', 'o', '1', 'B', 'a'],
+        unmatched: vec!['r', '2'],
+        reached_eos: false
+    };
+
+    assert_eq!(result, expected);
+    Ok(())
+}
+
+#[test]
+fn should_match_exactly_3() -> Result<(), PipelineError<'static, char>>{
+    let result = begin_match("18a18b18c")
+    .with_quantifier(Exactly(NonZeroUsize::new(3).unwrap()), |p|{
+        p.match_pattern(&['1', '8'])?
+        .match_any_of(&['a', 'b', 'c'])
+    })?;
+
+    let expected = MatchingPipeline {
+        matched: vec!['1','8','a', '1','8','b', '1','8','c'],
+        unmatched: vec![],
+        reached_eos: true
+    };
+
+    assert_eq!(result, expected);
+
+    Ok(())
+}
+
+#[test]
+fn quantifier_exactly_do_not_match() -> Result<(), PipelineError<'static, char>>{
+    let result = begin_match("abaabaO")
+    .with_quantifier(Exactly(NonZeroUsize::new(3).unwrap()), |p| {
+        p.match_pattern(&['a', 'b', 'a'])
+    });
+
+    let expected = Err(PipelineError::WrongPattern { expected: &['a', 'b', 'a'], actual: vec!['O'] });
+
+    assert_eq!(result, expected);
+    Ok(())
+}
